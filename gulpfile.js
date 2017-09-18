1c77d09b4bc4cdd089fa97a550429dbf481704ad
Łukasz Czulak <lukasz.czulak@gmail.com>
2017-09-18 12:47:18 +0200

    Add Simple WP Theme + Gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp'),
+ sass = require('gulp-sass'),
+ autoprefixer = require('gulp-autoprefixer'),
+ minify = require('gulp-minify-css'),
+ gutil = require('gulp-util'),
+ del = require('del');
+
+gulp.task('sass', function () {
+    gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minify())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('scss:watch', function () {
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('clean', function(cb) {
+    del(['.styles/**'], cb);
+});
+
+gulp.task('default', function() {
+    gulp.start('clean');
+    gulp.start('scss:watch');
+});
