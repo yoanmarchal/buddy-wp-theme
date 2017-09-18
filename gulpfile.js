a3bfd7d6b36edd7340982c80f89617e962b10ba4
Łukasz Czulak <lukasz.czulak@gmail.com>
2017-09-18 14:00:07 +0200

    ad AutoPrefixer to Gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var gulp = require('gulp'),
 gulp.task('sass', function () {
     gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['Android >= 2.3', 'BlackBerry >= 7', 'Chrome >= 9', 'Firefox >= 4', 'Explorer >= 9', 'iOS >= 5', 'Opera >= 11', 'Safari >= 5', 'OperaMobile >= 11', 'OperaMini >= 6', 'ChromeAndroid >= 9', 'FirefoxAndroid >= 4', 'ExplorerMobile >= 9'],
+            cascade: false
+        }))
         .pipe(minify())
         .pipe(gulp.dest('./css'));
 });
